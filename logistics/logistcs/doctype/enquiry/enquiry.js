9ea8a1efff84a169ea5613a1d196a692bee30b6e
Laxman <laxmantandon@gmail.com>
2024-09-19 11:55:04 +0530

    workspace updated

--- logistics/logistcs/doctype/enquiry/enquiry.js
+++ logistics/logistcs/doctype/enquiry/enquiry.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Enquiry', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		if (frm.doc.docstatus == 1) {
+			frm.add_custom_button('Costing', function() {
+				
+				let costing = frappe.model.get_new_doc('Costing');
+					
+				costing.enquiry = frm.doc.name
+				costing.posting_date = frappe.datetime.get_today()
+				costing.naming_series = 'C-2425-.####'
+			 										
+				frappe.set_route('form', 'Costing', costing.name);
+			}, "Create");
+		}
 
-	// }
+	}
 });
